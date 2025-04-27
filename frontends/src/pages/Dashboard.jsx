04c1289fa65e7ae31265d1ce7e3825555c8e55db
sarikap9 <sarika@platform9.com>
2025-04-27 14:53:36 +0530

    added sidebar contents

---
+++ frontends/src/pages/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Typography } from '@mui/material';
+
+function Dashboard() {
+  return (
+    <div>
+      <Typography variant="h4" gutterBottom>Dashboard</Typography>
+      {/* We will add cards and charts here next! */}
+    </div>
+  );
+}
+
+export default Dashboard;
